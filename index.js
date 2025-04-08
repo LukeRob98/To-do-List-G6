c5fea8fd12f76e8242744c26432cd46e803b1cde
luke <lukeu571@outlook.com>
2025-04-08 15:26:30 -0500

    Project Update

---
+++ index.js
@@ -0,0 +1,17 @@
+const addTaskButton = document.getElementById('addTaskBtn');
+
+addTaskButton.addEventListener('click', function () {
+  let input = document.getElementById('textInput');
+  let task = input.value;
+  if (task !== "") {
+  	var ul = document.getElementById('taskList');
+    var newTask = document.createElement('li');
+    newTask.textContent = task;
+    ul.appendChild(newTask);
+    input.value = '';
+  } else {
+  	alert('Please enter task');
+  }
+});
+
+
